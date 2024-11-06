30d157d74ba769b146b3b2309f5bf8b398d2f5b1
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-06 13:08:01 +0530

    changes in service interface

--- src/main/java/com/example/demo/service/AccountService.java
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -1,8 +1,20 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import com.example.demo.data.AccountsData;
 
 public interface AccountService {
 	
 	AccountsData createAccount(AccountsData accountsdata);
+	
+	AccountsData getAccountByid(int id);
+	
+	AccountsData deposit(int id, double amount);
+	
+	AccountsData withdraw(int id, double amount);
+	
+	List<AccountsData> getAll();
+	
+	void deleteAccount(int id);
 }
