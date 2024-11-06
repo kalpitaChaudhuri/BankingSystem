8e4cbd214aa5d00192f8b568fe67d987641b0acc
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-06 13:54:01 +0530

    changes in service interface (2)

--- src/main/java/com/example/demo/service/AccountServiceImplementation.java
+++ src/main/java/com/example/demo/service/AccountServiceImplementation.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +21,36 @@ public class AccountServiceImplementation implements AccountService{
 		Accounts acc=AccountMapper.maptoaccount(accountsdata);
 		Accounts datasaved=accrepo.save(acc);
 		return AccountMapper.maptoaccountdetails(datasaved);
+	}
+
+	@Override
+	public AccountsData getAccountByid(int id) {
+		Accounts account=accrepo.findById(id).orElseThrow(() ->new  RuntimeException("id does not exists"));
+		return AccountMapper.maptoaccountdetails(account);
+	}
+
+	@Override
+	public AccountsData deposit(int id, double amount) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AccountsData withdraw(int id, double amount) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AccountsData> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAccount(int id) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
