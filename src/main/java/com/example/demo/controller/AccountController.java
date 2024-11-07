a3902006eebe9c658e2564ea9707628a9dc9793f
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-07 12:18:58 +0530

    get method added in controller

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +32,11 @@ public class AccountController {
 	public ResponseEntity<AccountsData> addaccount(@RequestBody AccountsData accountsdata) {
 		//System.out.println("Triggered");
 		return new ResponseEntity<AccountsData>(accountservice.createAccount(accountsdata), HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<AccountsData> getAccountByid(@PathVariable int id) {
+		AccountsData accountsdata = accountservice.getAccountById(id);
+		return ResponseEntity.ok(accountsdata);
 	}
 }
