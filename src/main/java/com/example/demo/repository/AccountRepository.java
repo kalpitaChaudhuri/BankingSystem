3eff0ab2fd42c14607ec73058e7a253997b50afd
kalpitaChaudhuri <kalpitachaudhuri01@gmail.com>
2024-11-08 16:51:20 +0530

    updates in acc repo

--- src/main/java/com/example/demo/repository/AccountRepository.java
+++ src/main/java/com/example/demo/repository/AccountRepository.java
@@ -1,13 +1,9 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.Accounts;
 
 public interface AccountRepository extends JpaRepository<Accounts, Integer>{
 
-	public List<Accounts> findByName(String name);
-	public List<Accounts> findByAge(int age);
 }
